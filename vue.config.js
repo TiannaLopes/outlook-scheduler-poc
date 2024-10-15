766153c7a37b4737b766cc54d554a823ce3a2ccd
Tianna Lopes <63357829+TiannaLopes@users.noreply.github.com>
2024-10-14 22:05:31 -0400

    Fix public path for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/outlook-scheduler-poc/' : '/',
+});
